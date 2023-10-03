7583028a9ae30054aa190c0ac9ad0fa2d94b9893
Eric Bolikowski <eric@binarylights.com>
2023-10-03 18:59:08 +0200

    OkResponseDto: fix boolean mistake

--- 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/libs/api/ok.response.dto.ts
+++ 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/libs/api/ok.response.dto.ts
@@ -1,7 +1,7 @@
 import { ResponseDto } from "./response.dto.abstract";
 
 export class OkResponseDto<T> extends ResponseDto {
-  public readonly success = false;
+  public readonly success = true;
   public readonly error = undefined;
   constructor(httpStatusCode: number, readonly data: T) {
     super(httpStatusCode);
