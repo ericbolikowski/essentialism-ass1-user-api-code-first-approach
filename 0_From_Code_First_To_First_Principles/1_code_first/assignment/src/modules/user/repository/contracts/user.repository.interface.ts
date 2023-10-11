9bf8ba8b9a6249257539f2cd1674c5bd13e4c70c
Eric Bolikowski <eric@binarylights.com>
2023-10-11 14:50:54 +0200

    add jsdoc comments to user repo interface

--- 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/modules/user/repository/contracts/user.repository.interface.ts
+++ 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/modules/user/repository/contracts/user.repository.interface.ts
@@ -8,16 +8,42 @@ export type UserRepositoryCreateUserError =
 export type UserRepositoryGetUserError = "USER_NOT_FOUND" | "GENERIC";
 export type UserRepositoryEditUserError = "GENERIC";
 
+/**
+ * Interface for the User Repository, which defines methods for interacting with user data.
+ */
 export interface IUserRepository {
+  /**
+   * Creates a new user in the database.
+   * @param user - The user entity to create.
+   * @returns A Promise that resolves to a Result containing the created user entity or an error.
+   */
   createUser(
     user: UserEntity
   ): Promise<Result<UserEntity, UserRepositoryCreateUserError>>;
+
+  /**
+   * Retrieves a user from the database by their email address.
+   * @param email - The email address of the user to retrieve.
+   * @returns A Promise that resolves to a Result containing the retrieved user entity or an error.
+   */
   getUserByEmail(
     email: string
   ): Promise<Result<UserEntity, UserRepositoryGetUserError>>;
+
+  /**
+   * Retrieves a user from the database by their ID.
+   * @param id - The ID of the user to retrieve.
+   * @returns A Promise that resolves to a Result containing the retrieved user entity or an error.
+   */
   getUserById(
     id: number
   ): Promise<Result<UserEntity, UserRepositoryGetUserError>>;
+
+  /**
+   * Updates an existing user in the database.
+   * @param user - The updated user entity.
+   * @returns A Promise that resolves to a Result containing the updated user entity or an error.
+   */
   editUser(
     user: UserEntity
   ): Promise<Result<UserEntity, UserRepositoryEditUserError>>;
