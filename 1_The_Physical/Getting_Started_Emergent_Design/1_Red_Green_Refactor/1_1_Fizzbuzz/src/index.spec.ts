fde706ce93b794a22be719a8aaf44c257674718c
Eric Bolikowski <eric@binarylights.com>
2023-11-06 18:49:03 +0100

    [red] fails to return FizzBuzz given 15

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -8,4 +8,8 @@ describe("fizzbuzz", () => {
   it ("should return Buzz given 5", () => {
     expect(fizzbuzz(5)).toBe("Buzz")
   })
+
+  it("should return FizzBuzz given 15", () => {
+    expect(fizzbuzz(15)).toBe("FizzBuzz")
+  })
 });
