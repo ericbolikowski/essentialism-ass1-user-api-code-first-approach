c98e9f0b6dafadbf6b828219961a929eef57cdce
Eric Bolikowski <eric@binarylights.com>
2023-11-06 19:17:55 +0100

    [green] correctly throws error given -12

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,5 +1,5 @@
 export function fizzbuzz(n: number) {
-  if (n > 100) throw new Error("Number must be between 1 and 100")
+  if (n < 1 || n > 100) throw new Error("Number must be between 1 and 100")
   const isFizz = n % 3 === 0
   const isBuzz = n % 5 === 0
   if (isFizz && isBuzz) return "FizzBuzz"
