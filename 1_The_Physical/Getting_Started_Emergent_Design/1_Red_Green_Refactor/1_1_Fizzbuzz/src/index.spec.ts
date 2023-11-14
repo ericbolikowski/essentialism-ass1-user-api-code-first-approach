3743f5c56e0d34f2243f20e76d29bb4e463bb751
Eric Bolikowski <eric@binarylights.com>
2023-11-14 18:13:04 +0100

    [green] correctly throws an error given a non-number, such as NaN

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -56,4 +56,8 @@ describe("fizzbuzz", () => {
   it("should throw an error given any non-number, such as undefined", () => {
     expect(() => fizzbuzz(undefined as any)).toThrowError()
   })
+
+  it("should throw an error given any non-number, such as NaN", () => {
+    expect(() => fizzbuzz(NaN as any)).toThrowError()
+  })
 });
