2a3eca93a64544923c37ab2938c7acb2486c000b
Eric Bolikowski <eric@binarylights.com>
2023-11-14 20:53:58 +0100

    [refactor] test cases that return Fizz

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,91 +1,87 @@
-import { fizzbuzz } from "./fizzbuzz"
+import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-  it("should return Fizz given 3", () => {
-    expect(fizzbuzz(3)).toBe("Fizz")
-  })
-  
-  it ("should return Buzz given 5", () => {
-    expect(fizzbuzz(5)).toBe("Buzz")
-  })
+  it.each([3, 9, 42])("returns Fizz given %p", (n: number) =>
+    expect(fizzbuzz(n)).toBe("Fizz")
+  );
+
+  it("should return Buzz given 5", () => {
+    expect(fizzbuzz(5)).toBe("Buzz");
+  });
 
   it("should return FizzBuzz given 15", () => {
-    expect(fizzbuzz(15)).toBe("FizzBuzz")
-  })
+    expect(fizzbuzz(15)).toBe("FizzBuzz");
+  });
 
-  it("should return Fizz given 9", () => {
-    expect(fizzbuzz(9)).toBe("Fizz")
-  })
+  it("should return FizzBuzz given 45", () => {
+    expect(fizzbuzz(45)).toBe("FizzBuzz");
+  });
 
-  it ("should return Fizz given 42", () => {
-    expect(fizzbuzz(42)).toBe("Fizz")
-  })
+  it("should return '43' given 43", () => {
+    expect(fizzbuzz(43)).toBe("43");
+  });
 
-  it ("should return FizzBuzz given 45", () => {
-    expect(fizzbuzz(45)).toBe("FizzBuzz")
-  })
+  it("should throw an error given 102", () => {
+    expect(() => fizzbuzz(102)).toThrowError(
+      "Number must be between 1 and 100"
+    );
+  });
 
-  it ("should return '43' given 43", () => {
-    expect(fizzbuzz(43)).toBe("43")
-  })
+  it("should throw an error given -12", () => {
+    expect(() => fizzbuzz(-12)).toThrowError(
+      "Number must be between 1 and 100"
+    );
+  });
 
-  it ("should throw an error given 102", () => {
-    expect(() => fizzbuzz(102)).toThrowError("Number must be between 1 and 100")
-  })
-
-  it ("should throw an error given -12", () => {
-    expect(() => fizzbuzz(-12)).toThrowError("Number must be between 1 and 100")
-  })
-
-  it ("should throw an error given any non-number, such as string", () => {
-    expect(() => fizzbuzz("hello" as any)).toThrowError()
-  })
+  it("should throw an error given any non-number, such as string", () => {
+    expect(() => fizzbuzz("hello" as any)).toThrowError();
+  });
 
   it("should throw an error given any non-number, such as true", () => {
-    expect(() => fizzbuzz(true as any)).toThrowError()
-  })    
+    expect(() => fizzbuzz(true as any)).toThrowError();
+  });
 
   it("should throw an error given any non-number, such as object", () => {
-    expect(() => fizzbuzz({} as any)).toThrowError()
-  })
+    expect(() => fizzbuzz({} as any)).toThrowError();
+  });
 
   it("should throw an error given any non-number, such as null", () => {
-    expect(() => fizzbuzz(null as any)).toThrowError()
-  })
+    expect(() => fizzbuzz(null as any)).toThrowError();
+  });
 
   it("should throw an error given any non-number, such as undefined", () => {
-    expect(() => fizzbuzz(undefined as any)).toThrowError()
-  })
+    expect(() => fizzbuzz(undefined as any)).toThrowError();
+  });
 
   it("should throw an error given any non-number, such as NaN", () => {
-    expect(() => fizzbuzz(NaN as any)).toThrowError()
-  })
+    expect(() => fizzbuzz(NaN as any)).toThrowError();
+  });
 
   it("should throw an error given any non-number, such as Infinity", () => {
-    expect(() => fizzbuzz(Infinity as any)).toThrowError()
-  })
+    expect(() => fizzbuzz(Infinity as any)).toThrowError();
+  });
 
   it("should throw an error given any non-number, such as -Infinity", () => {
-    expect(() => fizzbuzz(-Infinity as any)).toThrowError()
-  })
+    expect(() => fizzbuzz(-Infinity as any)).toThrowError();
+  });
 
   it("should throw an error given any non-number, such as Symbol", () => {
-    expect(() => fizzbuzz(Symbol() as any)).toThrowError()
-  })
+    expect(() => fizzbuzz(Symbol() as any)).toThrowError();
+  });
 
   it("should throw an error given any non-number, such as function", () => {
-    expect(() => fizzbuzz((() => {}) as any)).toThrowError()
-  })
+    expect(() => fizzbuzz((() => {}) as any)).toThrowError();
+  });
 
   it("should throw an error given any non-number, such as array", () => {
-    expect(() => fizzbuzz([] as any)).toThrowError()
-  })
+    expect(() => fizzbuzz([] as any)).toThrowError();
+  });
 
   it("should always return a string value given valid input, such as 5", () => {
-    expect(typeof fizzbuzz(5)).toBe("string")
-  })
+    expect(typeof fizzbuzz(5)).toBe("string");
+  });
 
   it("should always return a string value given valid input, such as 100", () => {
-    expect(typeof fizzbuzz(100)).toBe("string")
-  })
+    expect(typeof fizzbuzz(100)).toBe("string");
+  });
 });
