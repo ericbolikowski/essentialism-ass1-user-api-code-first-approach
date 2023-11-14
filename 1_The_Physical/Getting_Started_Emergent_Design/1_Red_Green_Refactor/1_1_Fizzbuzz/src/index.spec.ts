4801edbc0f2579007c3e12bd326c218982a4ab68
Eric Bolikowski <eric@binarylights.com>
2023-11-14 18:15:25 +0100

    [green] correctly throws an error given a non-number, such as Symbol

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -68,4 +68,8 @@ describe("fizzbuzz", () => {
   it("should throw an error given any non-number, such as -Infinity", () => {
     expect(() => fizzbuzz(-Infinity as any)).toThrowError()
   })
+
+  it("should throw an error given any non-number, such as Symbol", () => {
+    expect(() => fizzbuzz(Symbol() as any)).toThrowError()
+  })
 });
