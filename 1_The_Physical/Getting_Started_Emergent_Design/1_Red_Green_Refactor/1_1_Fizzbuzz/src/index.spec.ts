dff8ae6a091670b1cca7048c566c5930e7074ed0
Eric Bolikowski <eric@binarylights.com>
2023-11-17 03:40:08 +0100

    [refactor] improve test descriptions

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,25 +1,37 @@
 import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-  it.each([3, 9, 42])("should return Fizz given %p", (n: number) =>
-    expect(fizzbuzz(n)).toBe("Fizz")
+  it.each([3, 9, 42])(
+    "should return Fizz given a multiple of 3, such as %p",
+    (n: number) => expect(fizzbuzz(n)).toBe("Fizz")
   );
 
-  it.each([5, 20, 50, 100])("should return Buzz given %p", (n: number) => {
-    expect(fizzbuzz(n)).toBe("Buzz");
-  });
+  it.each([5, 20, 50, 100])(
+    "should return Buzz given a multiple of 5, such as %p",
+    (n: number) => {
+      expect(fizzbuzz(n)).toBe("Buzz");
+    }
+  );
 
-  it.each([15, 45, 90])("should return FizzBuzz given %p", (n: number) => {
-    expect(fizzbuzz(n)).toBe("FizzBuzz");
-  });
+  it.each([15, 45, 90])(
+    "should return FizzBuzz given a multiple of both 3 and 5, such as %p",
+    (n: number) => {
+      expect(fizzbuzz(n)).toBe("FizzBuzz");
+    }
+  );
 
-  it("should return '43' given 43", () => {
+  it("should return the string '43' given the number 43", () => {
     expect(fizzbuzz(43)).toBe("43");
   });
 
-  it.each([-12, 102])("should throw an error given %p", (n: number) => {
-    expect(() => fizzbuzz(n)).toThrowError("Number must be between 1 and 100");
-  });
+  it.each([-12, 102])(
+    "should throw an error given an out-of-[1,100]-range value, such as %p",
+    (n: number) => {
+      expect(() => fizzbuzz(n)).toThrowError(
+        "Number must be between 1 and 100"
+      );
+    }
+  );
 
   it.each([
     "hello",
@@ -33,7 +45,7 @@ describe("fizzbuzz", () => {
     Symbol(),
     () => {},
     [],
-  ])("should throw an error given %p", (n: any) => {
+  ])("should throw an error given any non-number, such as %p", (n: any) => {
     expect(() => fizzbuzz(n)).toThrowError();
   });
 
