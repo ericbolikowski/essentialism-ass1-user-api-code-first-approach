b2c913d0dd426c7f199b150cd75449d19a25f31d
Eric Bolikowski <eric@binarylights.com>
2023-11-17 02:56:54 +0100

    [chore] format with prettier

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,14 +1,14 @@
 export function fizzbuzz(n: number) {
-  if (!isNumber(n)) throw new Error("Parameter `n` must be a number")
-  if (n < 1 || n > 100) throw new Error("Number must be between 1 and 100")
-  const isFizz = n % 3 === 0
-  const isBuzz = n % 5 === 0
-  if (isFizz && isBuzz) return "FizzBuzz"
-  if (isFizz) return "Fizz"
-  if (isBuzz) return "Buzz"
-  return n.toString()
+  if (!isNumber(n)) throw new Error("Parameter `n` must be a number");
+  if (n < 1 || n > 100) throw new Error("Number must be between 1 and 100");
+  const isFizz = n % 3 === 0;
+  const isBuzz = n % 5 === 0;
+  if (isFizz && isBuzz) return "FizzBuzz";
+  if (isFizz) return "Fizz";
+  if (isBuzz) return "Buzz";
+  return n.toString();
 }
 
 function isNumber(n: any): n is number {
-  return typeof n === "number" && !isNaN(n)
+  return typeof n === "number" && !isNaN(n);
 }
